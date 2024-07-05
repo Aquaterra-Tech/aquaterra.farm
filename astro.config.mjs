18ace0357dded4f2d0cd9b127394f1ee5baa2d21
João Justo <jpjustonunes@gmail.com>
2024-07-05 17:34:25 +0100

    Revert changes to astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aquaterra-tech.github.io',
-  base: 'aquaterra.farm',
+  // site: 'https://aquaterra-tech.github.io',
+  // base: 'aquaterra.farm',
 });
